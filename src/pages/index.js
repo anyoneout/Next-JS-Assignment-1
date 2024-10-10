6dd6655b043dcd0e203a507b32e75c498626cbe7
anyoneout <chrisdafur@gmail.com>
2024-10-10 18:23:38 -0500

    intialization

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
             <li>
               Get started by editing <code>src/pages/index.js</code>.
             </li>
-            <li>Save and see your changes instantly.</li>
+            <li>"So, this ride is safe?"</li>
           </ol>
 
           <div className={styles.ctas}>
